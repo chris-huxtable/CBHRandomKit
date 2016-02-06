71424bd500eb685f6689bfce2d0a33b2572fbb39
Chris Huxtable <chris@huxtable.ca>
2016-02-06 13:22:45 -0500

    Tabs...

--- CHRandomKit/NSNumber+CHRandomKit.m
+++ CHRandomKit/NSNumber+CHRandomKit.m
@@ -3,7 +3,7 @@
 //	CHRandomKit
 //
 //	Created by Christian Huxtable, October 2015.
-//  Copyright (c) 2015, Christian Huxtable <chris@huxtable.ca>
+//	Copyright (c) 2015, Christian Huxtable <chris@huxtable.ca>
 //
 //	Permission to use, copy, modify, and/or distribute this software for any purpose with or without
 //	fee is hereby granted, provided that the above copyright notice and this permission notice
