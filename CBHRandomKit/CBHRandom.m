6eeef13d216305daf0eb18ca2810fe1549336dbb
Chris Huxtable <chris@huxtable.ca>
2019-07-15 21:33:27 -0400

    Modularize Guards

--- CBHRandomKit/CBHRandom.m
+++ CBHRandomKit/CBHRandom.m
@@ -20,13 +20,22 @@
 
 #import "CBHRandom.h"
 
-#include <stdlib.h>
+#import <stdlib.h>
 #import <stdio.h>
 
 
 #define SWAP(T, a, b) do { T tmp = a; a = b; b = tmp; } while (0)
 #define MODULO_BIAS_UNSIGNED_MIN(T, S, bound) (T)(-(S)bound % (S)bound);
 
+#define _guardSameBounds(aLower, aUpper) if ( (aLower) == (aUpper) ) { return (aLower); }
+#define _guardSwappedBounds(aType, aLower, aUpper) if ( (aLower) > (aUpper) ) { SWAP(aType, (aLower), (aUpper)); }
+#define _quardNegativeBound(aBound) if ( (aBound) < 1 ) { return 0; }
+
+#define _guardBounds(aType, aLower, aUpper)\
+{\
+	_guardSameBounds(aLower, aUpper)\
+	_guardSwappedBounds(aType, aLower, aUpper)\
+}
 
 @implementation CBHRandom
 
@@ -79,8 +88,7 @@
 
 + (char)randomCharBetweenLower:(char)lowerBound andUpperBound:(char)upperBound
 {
-	if ( lowerBound == upperBound ) { return lowerBound; }
-	if ( lowerBound > upperBound ) { SWAP(char, lowerBound, upperBound); }
+	_guardBounds(char, lowerBound, upperBound);
 
 	unsigned char width = (unsigned char)(upperBound - lowerBound);
 	return (char)[self randomUnsignedByteWithBound:width] + lowerBound;
@@ -99,8 +107,7 @@
 
 + (int8_t)randomByteBetweenLower:(int8_t)lowerBound andUpperBound:(int8_t)upperBound
 {
-	if ( lowerBound == upperBound ) { return lowerBound; }
-	if ( lowerBound > upperBound ) { SWAP(int8_t, lowerBound, upperBound); }
+	_guardBounds(int8_t, lowerBound, upperBound);
 
 	uint8_t width = (uint8_t)(upperBound - lowerBound);
 	return (int8_t)[self randomUnsignedByteWithBound:width] + lowerBound;
@@ -117,7 +124,7 @@
 
 + (uint8_t)randomUnsignedByteWithBound:(uint8_t)bound
 {
-	if ( bound < 1 ) { return 0; }
+	_quardNegativeBound(bound);
 	++bound;
 
 	uint8_t min = MODULO_BIAS_UNSIGNED_MIN(uint8_t, int8_t, bound);
@@ -131,8 +138,7 @@
 
 + (uint8_t)randomUnsignedByteBetweenLower:(uint8_t)lowerBound andUpperBound:(uint8_t)upperBound
 {
-	if ( lowerBound == upperBound ) { return lowerBound; }
-	if ( lowerBound > upperBound ) { SWAP(uint8_t, lowerBound, upperBound); }
+	_guardBounds(uint8_t, lowerBound, upperBound);
 
 	uint8_t width = (uint8_t)(upperBound - lowerBound);
 	return [self randomUnsignedByteWithBound:width] + lowerBound;
@@ -152,8 +158,7 @@
 
 + (short)randomShortBetweenLower:(short)lowerBound andUpperBound:(short)upperBound
 {
-	if ( lowerBound == upperBound ) { return lowerBound; }
-	if ( lowerBound > upperBound ) { SWAP(short, lowerBound, upperBound); }
+	_guardBounds(short, lowerBound, upperBound);
 
 	unsigned short width = (unsigned short)(upperBound - lowerBound);
 	return (short)[self randomUnsignedShortWithBound:width] + lowerBound;
@@ -170,7 +175,7 @@
 
 + (unsigned short)randomUnsignedShortWithBound:(unsigned short)bound
 {
-	if ( bound < 1 ) { return 0; }
+	_quardNegativeBound(bound);
 	++bound;
 
 	unsigned short min = MODULO_BIAS_UNSIGNED_MIN(unsigned short, short, bound);
@@ -184,8 +189,7 @@
 
 + (unsigned short)randomUnsignedShortBetweenLower:(unsigned short)lowerBound andUpperBound:(unsigned short)upperBound
 {
-	if ( lowerBound == upperBound ) { return lowerBound; }
-	if ( lowerBound > upperBound ) { SWAP(unsigned short, lowerBound, upperBound); }
+	_guardBounds(unsigned short, lowerBound, upperBound);
 
 	unsigned short width = (unsigned short)(upperBound - lowerBound);
 	return [self randomUnsignedShortWithBound:width] + lowerBound;
@@ -204,8 +208,7 @@
 
 + (int)randomIntBetweenLower:(int)lowerBound andUpperBound:(int)upperBound
 {
-	if ( lowerBound == upperBound ) { return lowerBound; }
-	if ( lowerBound > upperBound ) { SWAP(int, lowerBound, upperBound); }
+	_guardBounds(int, lowerBound, upperBound);
 
 	unsigned int width = (unsigned int)(upperBound - lowerBound);
 	return (int)[self randomUnsignedIntWithBound:width] + lowerBound;
@@ -222,7 +225,7 @@
 
 + (unsigned int)randomUnsignedIntWithBound:(unsigned int)bound
 {
-	if ( bound < 1 ) { return 0; }
+	_quardNegativeBound(bound);
 	++bound;
 
 	unsigned int min = MODULO_BIAS_UNSIGNED_MIN(unsigned int, int, bound);
@@ -236,8 +239,7 @@
 
 + (unsigned int)randomUnsignedIntBetweenLower:(unsigned int)lowerBound andUpperBound:(unsigned int)upperBound
 {
-	if ( lowerBound == upperBound ) { return lowerBound; }
-	if ( lowerBound > upperBound ) { SWAP(unsigned int, lowerBound, upperBound); }
+	_guardBounds(unsigned int, lowerBound, upperBound);
 
 	unsigned int width = (unsigned int)(upperBound - lowerBound);
 	return [self randomUnsignedIntWithBound:width] + lowerBound;
@@ -256,8 +258,7 @@
 
 + (NSInteger)randomIntegerBetweenLower:(NSInteger)lowerBound andUpperBound:(NSInteger)upperBound
 {
-	if ( lowerBound == upperBound ) { return lowerBound; }
-	if ( lowerBound > upperBound ) { SWAP(NSInteger, lowerBound, upperBound); }
+	_guardBounds(NSInteger, lowerBound, upperBound);
 
 	NSUInteger width = (NSUInteger)(upperBound - lowerBound);
 	return (NSInteger)[self randomUnsignedIntegerWithBound:width] + lowerBound;
@@ -274,7 +275,7 @@
 
 + (NSUInteger)randomUnsignedIntegerWithBound:(NSUInteger)bound
 {
-	if ( bound < 1 ) { return 0; }
+	_quardNegativeBound(bound);
 	++bound;
 
 	NSUInteger min = MODULO_BIAS_UNSIGNED_MIN(NSUInteger, NSInteger, bound);
@@ -288,8 +289,7 @@
 
 + (NSUInteger)randomUnsignedIntegerBetweenLower:(NSUInteger)lowerBound andUpperBound:(NSUInteger)upperBound
 {
-	if ( lowerBound == upperBound ) { return lowerBound; }
-	if ( lowerBound > upperBound ) { SWAP(NSUInteger, lowerBound, upperBound); }
+	_guardBounds(NSUInteger, lowerBound, upperBound);
 
 	NSUInteger width = (NSUInteger)(upperBound - lowerBound);
 	return [self randomUnsignedIntegerWithBound:width] + lowerBound;
@@ -316,8 +316,7 @@
 
 + (long)randomLongBetweenLower:(long)lowerBound andUpperBound:(long)upperBound
 {
-	if ( lowerBound == upperBound ) { return lowerBound; }
-	if ( lowerBound > upperBound ) { SWAP(long, lowerBound, upperBound); }
+	_guardBounds(long, lowerBound, upperBound);
 
 	unsigned long width = (unsigned long)(upperBound - lowerBound);
 	return (long)[self randomUnsignedLongWithBound:width] + lowerBound;
@@ -334,7 +333,7 @@
 
 + (unsigned long)randomUnsignedLongWithBound:(unsigned long)bound
 {
-	if ( bound < 1 ) { return 0; }
+	_quardNegativeBound(bound);
 	++bound;
 
 	unsigned long min = MODULO_BIAS_UNSIGNED_MIN(unsigned long, long, bound);
@@ -348,8 +347,7 @@
 
 + (unsigned long)randomUnsignedLongBetweenLower:(unsigned long)lowerBound andUpperBound:(unsigned long)upperBound
 {
-	if ( lowerBound == upperBound ) { return lowerBound; }
-	if ( lowerBound > upperBound ) { SWAP(unsigned long, lowerBound, upperBound); }
+	_guardBounds(unsigned long, lowerBound, upperBound);
 
 	unsigned long width = (unsigned long)(upperBound - lowerBound);
 	return [self randomUnsignedLongWithBound:width] + lowerBound;
@@ -368,8 +366,7 @@
 
 + (long long)randomLongLongBetweenLower:(long long)lowerBound andUpperBound:(long long)upperBound
 {
-	if ( lowerBound == upperBound ) { return lowerBound; }
-	if ( lowerBound > upperBound ) { SWAP(long long, lowerBound, upperBound); }
+	_guardBounds(long long, lowerBound, upperBound);
 
 	unsigned long long width = (unsigned long long)(upperBound - lowerBound);
 	return (long long)[self randomUnsignedLongWithBound:width] + lowerBound;
@@ -386,7 +383,7 @@
 
 + (unsigned long long)randomUnsignedLongLongWithBound:(unsigned long long)bound
 {
-	if ( bound < 1 ) { return 0; }
+	_quardNegativeBound(bound);
 	++bound;
 
 	unsigned long long min = MODULO_BIAS_UNSIGNED_MIN(unsigned long long, long long, bound);
@@ -400,8 +397,7 @@
 
 + (unsigned long long)randomUnsignedLongLongBetweenLower:(unsigned long long)lowerBound andUpperBound:(unsigned long long)upperBound
 {
-	if ( lowerBound == upperBound ) { return lowerBound; }
-	if ( lowerBound > upperBound ) { SWAP(unsigned long long, lowerBound, upperBound); }
+	_guardBounds(unsigned long long, lowerBound, upperBound);
 
 	unsigned long long width = (unsigned long long)(upperBound - lowerBound);
 	return [self randomUnsignedLongWithBound:width] + lowerBound;
